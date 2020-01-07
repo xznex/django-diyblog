e3cab0185f1bba6afea6ca2d2fe66f1aaba4ad01
zne <zernin.egor2014@yandex.ru>
2020-01-07 18:21:07 +0500

    Created author, post and commentary models + admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Post, Author, Comment
 
-# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('post_date', 'title', 'author')
+    inlines = [CommentInline]
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+'''
+
+author > get_abs_url
+post > get_abs_url
+comment
+
+'''
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=3000, help_text='your description')
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    post_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField(max_length=2000, help_text='your bio')
+
+    def __str__(self):
+        return self.user.username
+
+
+class Comment(models.Model):
+    description = models.TextField(1000, help_text='your comment')
+    post_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        len_default = 75
+        if len(self.description) > len_default:
+            out_title = self.description[:len_default] + '...'
+        else:
+            out_title = self.description
+        return out_title
