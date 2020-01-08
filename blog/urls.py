7e36274f8cffa4bea8e0acc362cebb694a7f94ef
zne <zernin.egor2014@yandex.ru>
2020-01-09 00:19:34 +0500

    Created a display of lists of authors and blogs

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^bloggers/$', views.AuthorsList.as_view(), name='bloggers'),
+    url(r'^blogs/$', views.BlogsList.as_view(), name='blogs')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Post, Author, Comment
+from django.views import generic
 
 
 def index(request):
-    return render(request, 'index.html')
+    num_author = Author.objects.count()
+    num_post = Post.objects.all().count()
+    num_comment = Comment.objects.all().count()
+    return render(request, 'index.html', context={'num_author': num_author, 'num_post': num_post,
+                                                  'num_comment': num_comment})
+
+
+class AuthorsList(generic.ListView):
+    model = Author
+    # paginate_by = 10
+
+
+class BlogsList(generic.ListView):
+    model = Post
+    # paginate_by = 10
