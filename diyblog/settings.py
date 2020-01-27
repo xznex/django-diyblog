5bc9fb7d931f9035b0c96b5153d4d573e90a1bd9
zne <zernin.egor2014@yandex.ru>
2020-01-27 22:15:28 +0500

    Added allowed host in settings

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'e-pnz4^mgxc3@@u8dy##=kvfky5#e1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sleepy-thicket-27571.herokuapp.com']
 
 
 # Application definition
